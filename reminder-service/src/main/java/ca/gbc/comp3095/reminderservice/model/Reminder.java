0a98a2d51378cd5d8a04da45f68c4293804e0e27
Penny Ahlstrom <pennyahlstrom@hotmail.com>
2026-02-13 18:35:04 -0500

    updated reminder model

--- reminder-service/src/main/java/ca/gbc/comp3095/reminderservice/model/Reminder.java
+++ reminder-service/src/main/java/ca/gbc/comp3095/reminderservice/model/Reminder.java
@@ -1,122 +1,29 @@
 package ca.gbc.comp3095.reminderservice.model;
 
-
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import java.io.Serializable;
+import java.time.LocalDateTime;
 
 @Document(collection = "reminders")
-public class Reminder implements Serializable {
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Reminder {
 
     @Id
-    private String id;
+    private String id; // Mongo internal
 
-    private String title;
-    private String associatedTask;
-    private String course;
-    private String priority;
-    private String alertDate;
-    private String alertTime;
-    private String note;
+    private Long reminderId; // UML
 
-    private Integer taskId;
-    private Integer courseId;
+    private Long taskId;     // link to Task (microservice-friendly)
 
-    // Constructor
-    public Reminder(String title, String associatedTask, String course, String priority,
-                    String alertDate, String alertTime, String note, Integer taskId, Integer courseId){
-        this.title = title;
-        this.associatedTask = associatedTask;
-        this.course = course;
-        this.priority = priority;
-        this.alertDate = alertDate;
-        this.alertTime = alertTime;
-        this.note = note;
-        this.taskId = taskId;
-        this.courseId = courseId;
-    }
+    private String message;
 
-    // Getters and Setters
+    private LocalDateTime scheduledAt;
 
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getAssociatedTask() {
-        return associatedTask;
-    }
-
-    public void setAssociatedTask(String associatedTask) {
-        this.associatedTask = associatedTask;
-    }
-
-    public String getCourse() {
-        return course;
-    }
-
-    public void setCourse(String course) {
-        this.course = course;
-    }
-
-    public String getPriority() {
-        return priority;
-    }
-
-    public void setPriority(String priority) {
-        this.priority = priority;
-    }
-
-    public String getAlertDate() {
-        return alertDate;
-    }
-
-    public void setAlertDate(String alertDate) {
-        this.alertDate = alertDate;
-    }
-
-    public String getAlertTime() {
-        return alertTime;
-    }
-
-    public void setAlertTime(String alertTime) {
-        this.alertTime = alertTime;
-    }
-
-    public String getNote() {
-        return note;
-    }
-
-    public void setNote(String note) {
-        this.note = note;
-    }
-
-    public Integer getTaskId() {
-        return taskId;
-    }
-
-    public void setTaskId(Integer taskId) {
-        this.taskId = taskId;
-    }
-
-    public Integer getCourseId() {
-        return courseId;
-    }
-
-    public void setCourseId(Integer courseId) {
-        this.courseId = courseId;
-    }
+    private boolean wasSent;
 }

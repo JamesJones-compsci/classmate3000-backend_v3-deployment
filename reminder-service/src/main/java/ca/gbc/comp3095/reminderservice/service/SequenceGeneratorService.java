37c4af3c8c416bd96c93526005fac97590fd52cd
Penny Ahlstrom <pennyahlstrom@hotmail.com>
2026-02-13 18:33:47 -0500

    added sequence generator for mongodb ids

---
+++ reminder-service/src/main/java/ca/gbc/comp3095/reminderservice/service/SequenceGeneratorService.java
@@ -0,0 +1,33 @@
+package ca.gbc.comp3095.reminderservice.service;
+
+import ca.gbc.comp3095.reminderservice.model.DatabaseSequence;
+import org.springframework.data.mongodb.core.FindAndModifyOptions;
+import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SequenceGeneratorService {
+
+    private final MongoOperations mongoOperations;
+
+    public SequenceGeneratorService(MongoOperations mongoOperations) {
+        this.mongoOperations = mongoOperations;
+    }
+
+    public long getNextSequence(String seqName) {
+        Query query = new Query(Criteria.where("_id").is(seqName));
+        Update update = new Update().inc("seq", 1);
+
+        DatabaseSequence counter = mongoOperations.findAndModify(
+                query,
+                update,
+                FindAndModifyOptions.options().returnNew(true).upsert(true),
+                DatabaseSequence.class
+        );
+
+        return (counter != null) ? counter.getSeq() : 1L;
+    }
+}

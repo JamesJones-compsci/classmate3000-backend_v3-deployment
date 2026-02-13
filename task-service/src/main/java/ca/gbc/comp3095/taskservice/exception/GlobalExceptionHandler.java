f1d15e13ef23b44b8fdf7069c8182ecd3b4de6a3
Penny Ahlstrom <pennyahlstrom@hotmail.com>
2026-02-13 17:55:49 -0500

    added dependencies and updated GlobalExceptionHandler

--- task-service/src/main/java/ca/gbc/comp3095/taskservice/exception/GlobalExceptionHandler.java
+++ task-service/src/main/java/ca/gbc/comp3095/taskservice/exception/GlobalExceptionHandler.java
@@ -5,19 +5,29 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+// PENNY - Added these dependencies for @Valid used in controller
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import java.util.HashMap;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(TaskNotFoundException.class)
-    public ResponseEntity<String> handleTaskNotFound(TaskNotFoundException ex){
-        return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                .body(ex.getMessage());
+    public ResponseEntity<String> handleTaskNotFound(TaskNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handleValidation(MethodArgumentNotValidException ex) {
+        var errors = new HashMap<String, String>();
+        ex.getBindingResult().getFieldErrors()
+                .forEach(err -> errors.put(err.getField(), err.getDefaultMessage()));
+        return ResponseEntity.badRequest().body(errors);
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<String> handleGeneric(Exception ex){
+    public ResponseEntity<String> handleGeneric(Exception ex) {
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body(ex.getMessage());
+                .body("Internal server error");
     }
-
 }

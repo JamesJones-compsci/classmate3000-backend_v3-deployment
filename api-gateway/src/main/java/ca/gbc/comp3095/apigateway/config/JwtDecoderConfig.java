7c03c5a4e2238a9d86eda95bdbc3cab378ed21dc
Penny Ahlstrom <pennyahlstrom@hotmail.com>
2026-02-14 02:48:28 -0500

    setup Jwt decoder for gateway to validate using jwt secret

---
+++ api-gateway/src/main/java/ca/gbc/comp3095/apigateway/config/JwtDecoderConfig.java
@@ -0,0 +1,28 @@
+package ca.gbc.comp3095.apigateway.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusReactiveJwtDecoder;
+
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+
+@Configuration
+public class JwtDecoderConfig {
+
+    @Bean
+    public ReactiveJwtDecoder reactiveJwtDecoder(@Value("${jwt.secret}") String secret) {
+        SecretKeySpec key = new SecretKeySpec(
+                secret.getBytes(StandardCharsets.UTF_8),
+                "HmacSHA256"
+        );
+
+        return NimbusReactiveJwtDecoder
+                .withSecretKey(key)
+                .macAlgorithm(MacAlgorithm.HS256)
+                .build();
+    }
+}
